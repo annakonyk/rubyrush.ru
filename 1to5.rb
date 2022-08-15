222dd220ed91e93b96b6417501025ed571afff37
annakonyk <111083357+annakonyk@users.noreply.github.com>
2022-08-15 21:26:36 +0300

    Create 1to5.rb

---
+++ 1to5.rb
@@ -0,0 +1,15 @@
+array1 = [1, 2, 3, 4, 5]
+
+puts "Даний масив: " + array1.to_s
+
+puts array1[4]
+
+puts array1[3]
+
+puts array1[2]
+
+puts array1[1]
+
+puts array1[0]
+
+puts "Обернений масив: " + array1.reverse.to_s
